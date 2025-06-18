d31ce3d2067a8e83794b2b0562e29d88f07deb79
rodwell.leo <leorodwel86@gmail.com>
2025-06-18 13:10:21 +0300

    Updates CORS origin for UAT environment
    Modifies the CORS configuration to allow requests from the new UAT deployment URL.
    
    This change ensures that the application can communicate correctly with the UAT environment, improving the development and testing workflow.

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:8080", "https://avapay.vercel.ap"],
+        origin: ["http://localhost:8080", "https://uat-avapay.vercel.app"],
         methods: ["GET", "POST", "OPTIONS"],
     },
 });
