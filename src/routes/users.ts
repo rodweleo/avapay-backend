4a7934925e8c26664fd580a5ec7c7d02db3ea946
rodwell.leo <leorodwel86@gmail.com>
2025-06-24 08:49:54 +0300

    fix: update response message for users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,11 @@
 
 import express from "express"
-import { prisma } from "../utils/prisma";
 
 const UserRouter = express.Router();
 
 UserRouter.get("/", async (req, res) => {
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
     res.json({
-        message: "We are live"
+        message: "Users route is live"
     })
 })
 
